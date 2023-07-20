bdb6ff2ac5c581f17eb93ba28531549826771e56
punithnmnp1 <139468900+punithnmnp1@users.noreply.github.com>
2023-07-20 23:30:23 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ class Customer
 {
 public:
     string customer_Name;
+    string address;
     int car_model;
     string car_Number;
 };
@@ -25,19 +26,27 @@ private:
     int numberOfDays;
     int rent_Fee;
     char str[200];
-    bool value;		//A character array in which to store the data with Maximum number of characters to extract.
+    bool value;
+    bool ch;	//A character array in which to store the data with Maximum number of characters to extract.
 
-    unordered_map<string, Customer> customerRecords;  // Hash table to store customer records based on car numbers
+    unordered_map<string, Customer> customerRecords; // Hash table to store customer records based on car numbers
 
+    unordered_map<string, string> carIndex;
 public:
     Rent();
     void welcome();
     void fileReader(const char* carFile);
     void data();
     void invoiceAmount();	//Computes
-    void invoiceRecord();
+    void invoiceRecord();   //Prints the invoice
     void storeRentDetails();
-    void modify();      //Prints the invoice
+    void modify();
+    void addToIndex(const string& car_Number, const string& customerKey);
+    void removeFromIndex(const string& car_Number);
+    void displayIndex();
+    void storingRentDetails();
+    bool isCarNumberUnique(const string& car_Number);
+    void cancelBooking();
 };
 
 Rent::Rent()
@@ -81,11 +90,38 @@ void Rent::fileReader(const char* carFile)
     }
 }
 
+void Rent::addToIndex(const string& car_Number, const string& customerKey)
+{
+    carIndex[car_Number] = customerKey;
+}
+
+void Rent::removeFromIndex(const string& car_Number)
+{
+    carIndex.erase(car_Number);
+}
+void Rent::displayIndex()
+{
+    cout << "Car Index:" << endl;
+    for (const auto& pair : carIndex) {
+        cout << "Car Number: " << pair.first << ", Customer Key: " << pair.second << endl;
+    }
+    cout << endl;
+}
+
+bool Rent::isCarNumberUnique(const string& car_Number) {
+    ifstream inputFile("rental_details.txt");
+    string line;
+    while (getline(inputFile, line)) {
+        if (line.find(car_Number) != string::npos) {
+            return false;  // Car number already exists in the file
+        }
+    }
+    return true;  // Car number is unique
+}
+
 void Rent::data()
 {
-    cout << "\n\n\t\t\t\tPlease Enter your Name: "; //taking data from the user
-    cin >> customer_Name;
-    cout << endl;
+
     do {
         //giving user a choice to select among three different models
         cout << "\t\t\t\t__________________________________________________________________" << endl;
@@ -155,21 +191,39 @@ void Rent::data()
     cout<< "Are you sure u want to rent this car, click 1  for yes and click 0 for no:";
     cin>>value;
     if(value==1){
-    cout << "Please select a Car No. : ";
-    cin >> car_Number;
+    cout << "Please Enter your Name: "; //taking data from the user
+    cin >> customer_Name;
+    cout << "enter your address:";
+    cin >> address;
+    bool uniqueCarNumber = false;
+    do {
+        cout << "Please enter a Car No.: ";
+        cin >> car_Number;
+
+        if (isCarNumberUnique(car_Number)) {
+            uniqueCarNumber = true;
+        } else {
+            cout << "Car Number is already assigned. Please enter a unique Car No." << endl;
+        }
+    } while (!uniqueCarNumber);
     cout << "Number of days you wish to rent the car : ";
     cin >> numberOfDays;
     cout << endl;
     }else{
-    exit(0);
-    fileReader("thanks.txt");
+    system("CLS");
+     data();
+
     }
 
     // Store customer data in the hash table
     Customer customer;
     customer.customer_Name = customer_Name;
+    customer.address=address;
     customer.car_model = car_model;
     customer.car_Number = car_Number;
+    string customerKey = to_string(rand());  // Generate a random key for the customer record
+    customerRecords[customerKey] = customer;
+    addToIndex(car_Number, customerKey);
 
     customerRecords[car_Number] = customer;
 }
@@ -195,18 +249,23 @@ void Rent::invoiceAmount()
 }
 void Rent::modify()
 {
-    string carNumber;
+    system("CLS");
+    string car_Number;
+    cout<<"do you wish to modify (if yes enter 1 else enter 0):";
+    cin>>ch;
+    if(ch==1){
     cout << "Enter the Car Number of the customer you want to modify: ";
-    cin >> carNumber;
+    cin >> car_Number;
 
     // Check if the customer record exists in the hash table
-    if (customerRecords.find(carNumber) != customerRecords.end()) {
-        Customer& customer = customerRecords[carNumber];
+   if (carIndex.find(car_Number) != carIndex.end()) {
+        string customerKey = carIndex[car_Number];
+        Customer& customer = customerRecords[customerKey];
 
         // Display the current rental details
         cout << "Current Rental Details:" << endl;
         cout << "Customer Name: " << customer.customer_Name << endl;
-        cout << "Car Model: " << customer.car_model << endl;
+        cout << "Customer address :"<< customer.address<<endl;
         cout << "Car Number: " << customer.car_Number << endl;
         cout << "no.of days car rented: "<<numberOfDays<<endl;
 
@@ -214,20 +273,23 @@ void Rent::modify()
         cout << "Enter the new rental details:" << endl;
         cout << "Customer Name: ";
         cin >> customer.customer_Name;
-        cout << "Car Model: ";
-        cin >> customer.car_model;
+        cout << "Customer's address: ";
+        cin >>customer.address;
         cout<<"no.of days car rented: ";
         cin>>numberOfDays;
 
         // Update the rental details in the hash table
-        customerRecords[carNumber] = customer;
+        customerRecords[car_Number] = customer;
 
         cout << "Rental details modified successfully." << endl;
     } else {
         cout << "Customer record not found." << endl;
     }
 }
-
+else{
+ return;
+ }
+}
 
 void Rent::invoiceRecord()
 {
@@ -279,6 +341,7 @@ void Rent::storeRentDetails()
     // Write rent details to the
     outputFile << "Rent details generated at:"<< date << endl;
     outputFile << "Customer Name: " << customer_Name << endl;
+    outputFile << "Customer address:"<< address <<endl;
     outputFile << "Car Model: " << car_model << endl;
     outputFile << "Car Number: " << car_Number << endl;
     outputFile << "Number of Days: " << numberOfDays << endl;
@@ -291,6 +354,92 @@ void Rent::storeRentDetails()
     cout << "Rent details have been stored successfully." << endl;
     fileReader("thanks.txt");
 }
+ void Rent::storingRentDetails()
+{
+    ofstream outputFile;
+    time_t now = time(0);		//getting local time
+    char* date = ctime(&now);
+    outputFile.open("rental_details.txt", ios::app); // open the file in append mode
+
+    // Check if the file was opened successfully
+    if (!outputFile) {
+        cout << "Error in opening file for writing." << endl;
+        return;
+    }
+
+    // Write rent details to the file
+    outputFile << "bill generated at:"<< date << endl;
+    outputFile << "Customer Name: " << customer_Name << endl;
+    outputFile << "Address: " << address << endl;
+    outputFile << "Car Model: " << car_model << endl;
+    outputFile << "Car Number: " << car_Number << endl;
+    outputFile << "Number of Days: " << numberOfDays << endl;
+    outputFile << "Rent Fee: " << rent_Fee << endl;
+    outputFile << endl;
+
+    // Close the file
+    outputFile.close();
+
+
+}
+void Rent::cancelBooking()
+{
+    string car_Number;
+    cout << "Enter the Car Number of the customer you want to cancel the booking for: ";
+    cin >> car_Number;
+
+    // Check if the customer record exists in the hash table
+    if (carIndex.find(car_Number) != carIndex.end()) {
+        string customerKey = carIndex[car_Number];
+        Customer& customer = customerRecords[customerKey];
+
+        // Display the current rental details
+        cout << "Current Rental Details:" << endl;
+        cout << "Customer Name: " << customer.customer_Name << endl;
+        cout << "Customer address: " << customer.address << endl;
+        cout << "Car Number: " << customer.car_Number << endl;
+        cout << "Number of days car rented: " << numberOfDays << endl;
+
+        // Prompt the user for confirmation
+        char choice;
+        cout << "Are you sure you want to cancel this booking? (Y/N): ";
+        cin >> choice;
+
+        if (choice == 'Y' || choice == 'y') {
+            // Remove the rental details from the hash table and the carIndex
+            customerRecords.erase(customerKey);
+            removeFromIndex(car_Number);
+
+            // Update the rental_details.txt file to indicate the booking is canceled
+            ifstream inputFile("rental_details.txt");
+            ofstream outputFile("temp_rental_details.txt");
+
+            string line;
+            while (getline(inputFile, line)) {
+                if (line.find(car_Number) != string::npos) {
+                    // Add a marker/message to indicate the booking is canceled
+                    outputFile << line << " [Booking Canceled]" << endl;
+                } else {
+                    outputFile << line << endl;
+                }
+            }
+
+            inputFile.close();
+            outputFile.close();
+
+            // Remove the old rental_details.txt and rename temp_rental_details.txt to rental_details.txt
+            remove("rental_details.txt");
+            rename("temp_rental_details.txt", "rental_details.txt");
+
+            cout << "Booking for Car Number " << car_Number << " has been canceled." << endl;
+            exit(0); // Terminate the program after canceling the booking
+        } else {
+            cout << "Booking cancellation has been aborted." << endl;
+        }
+    } else {
+        cout << "Customer record not found." << endl;
+    }
+}
 
 
 int main()
@@ -302,6 +451,15 @@ int main()
     rent.modify();
     rent.invoiceAmount();
     rent.invoiceRecord();
+    rent.displayIndex();
     rent.storeRentDetails();
+    rent.storingRentDetails();
+    cout << "\nDo you want to cancel a car rental booking? (Y/N): ";
+    char cancelChoice;
+    cin >> cancelChoice;
+
+    if (cancelChoice == 'Y' || cancelChoice == 'y') {
+        rent.cancelBooking();
+    }
     return 0;
 }
